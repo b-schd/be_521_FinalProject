00e960e06788a0e18defc75ba993a290cf615d7f
Andy Revell <andyrevell93@gmail.com>
2019-04-16 19:21:13 -0400

    update

--- model_training.m
+++ model_training.m
@@ -2,13 +2,15 @@
 
 %%
 addpath(genpath('ieeg_matlabToolBox'));
-%IEEGSession works with my login if you add 'ieeg_matlabToolBox'to path
-s1_ecog_train = IEEGSession('I521_Sub1_Training_ecog', 'arevell', 'IEEGSession');
-s2_ecog_train = IEEGSession('I521_Sub2_Training_ecog', 'arevell', 'IEEGSession');
-s3_ecog_train = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'IEEGSession');
+%IEEGSession works with my login if you add 'ieeg_matlabToolBox' to path
+s1_train_ecog = IEEGSession('I521_Sub1_Training_ecog', 'arevell', 'IEEGSession');
+s2_train_ecog = IEEGSession('I521_Sub2_Training_ecog', 'arevell', 'IEEGSession');
+s3_train_ecog = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'IEEGSession');
 
-s1_dg_train = IEEGSession('I521_Sub1_Training_dg', 'arevell', 'IEEGSession');
-s2_dg_train = IEEGSession('I521_Sub2_Training_dg', 'arevell', 'IEEGSession');
-s3_dg_train = IEEGSession('I521_Sub3_Training_dg', 'arevell', 'IEEGSession');
+s1_train_dg = IEEGSession('I521_Sub1_Training_dg', 'arevell', 'IEEGSession');
+s2_train_dg = IEEGSession('I521_Sub2_Training_dg', 'arevell', 'IEEGSession');
+s3_train_dg = IEEGSession('I521_Sub3_Training_dg', 'arevell', 'IEEGSession');
 %%
-%COnfigure data to cell array
+%Configure data to cell array
+
+train_ecog = cell(3, 1)
