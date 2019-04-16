1f5cd9ec9ff0b42185ba9e65ea64e477f0dcf39e
Andy Revell <andyrevell93@gmail.com>
2019-04-16 19:03:40 -0400

    Update model_training.m

--- model_training.m
+++ model_training.m
@@ -1,7 +1,8 @@
 %Code to train and generate out model
 
-addpath('ieeg_matlabToolBox')
-
+%%
+addpath(genpath('ieeg_matlabToolBox'));
+%IEEGSession works with my login if you add 'ieeg_matlabToolBox'to path
 s1_ecog_train = IEEGSession('I521_Sub1_Training_ecog', 'arevell', 'are_ieeglogin.bin');
 s2_ecog_train = IEEGSession('I521_Sub2_Training_ecog', 'arevell', 'are_ieeglogin.bin');
 s3_ecog_train = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'are_ieeglogin.bin');
@@ -9,4 +10,5 @@ s3_ecog_train = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'are_ieeglogin
 s1_dg_train = IEEGSession('I521_Sub1_Training_dg', 'arevell', 'are_ieeglogin.bin');
 s2_dg_train = IEEGSession('I521_Sub2_Training_dg', 'arevell', 'are_ieeglogin.bin');
 s3_dg_train = IEEGSession('I521_Sub3_Training_dg', 'arevell', 'are_ieeglogin.bin');
-
+%%
+%COnfigure data to cell array
