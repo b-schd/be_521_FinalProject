78b37e1781ab4a62eac5d07d86af25ccbdc2bc69
Andy Revell <andyrevell93@gmail.com>
2019-04-16 18:41:54 -0400

    update

--- model_training.m
+++ model_training.m
@@ -1,1 +1,10 @@
 %Code to train and generate out model
+
+s1_ecog_train = IEEGSession('I521_Sub1_Training_ecog', 'arevell', 'are_ieeglogin.bin');
+s2_ecog_train = IEEGSession('I521_Sub2_Training_ecog', 'arevell', 'are_ieeglogin.bin');
+s3_ecog_train = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'are_ieeglogin.bin');
+
+
+s1_dg_train = IEEGSession('I521_Sub1_Training_dg', 'arevell', 'are_ieeglogin.bin');
+s2_dg_train = IEEGSession('I521_Sub2_Training_dg', 'arevell', 'are_ieeglogin.bin');
+s3_dg_train = IEEGSession('I521_Sub3_Training_dg', 'arevell', 'are_ieeglogin.bin');
