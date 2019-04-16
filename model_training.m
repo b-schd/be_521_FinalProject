3c6ef64b4ad6f42df72dc5ad678736ea42d9b18a
Andy Revell <andyrevell93@gmail.com>
2019-04-16 18:53:03 -0400

    update

--- model_training.m
+++ model_training.m
@@ -4,7 +4,7 @@ s1_ecog_train = IEEGSession('I521_Sub1_Training_ecog', 'arevell', 'are_ieeglogin
 s2_ecog_train = IEEGSession('I521_Sub2_Training_ecog', 'arevell', 'are_ieeglogin.bin');
 s3_ecog_train = IEEGSession('I521_Sub3_Training_ecog', 'arevell', 'are_ieeglogin.bin');
 
-
 s1_dg_train = IEEGSession('I521_Sub1_Training_dg', 'arevell', 'are_ieeglogin.bin');
 s2_dg_train = IEEGSession('I521_Sub2_Training_dg', 'arevell', 'are_ieeglogin.bin');
 s3_dg_train = IEEGSession('I521_Sub3_Training_dg', 'arevell', 'are_ieeglogin.bin');
+
